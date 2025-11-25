afc530dbec057ae6338c03197853f59d05071e76
vi17250 <v.aubinaud@protonmail.com>
2025-11-25 23:12:16 +0100

    fix: 🐛 Option has 3 properties
    Properties are: checked | highlighted | content

--- src/def.rs
+++ src/def.rs
@@ -1,44 +1,52 @@
 use std::fmt::{Display, Formatter, Result};
 
 #[derive(Debug, Copy, Clone)]
-pub struct Line<T: Display + Clone> {
+pub struct Option<T: Display + Clone> {
+    checked: bool,
     content: T,
-    is_selected: bool,
+    highlighted: bool,
 }
 
-impl<T: Display + Clone> Line<T> {
-    pub fn new(content: T) -> Self {
-        Line {
+impl<T: Display + Clone> Option<T> {
+    pub fn new(content: T, highlighted: bool) -> Self {
+        Option {
+            checked: false,
             content,
-            is_selected: false,
+            highlighted,
         }
     }
 }
 
-impl<T: Display + Clone> Line<T> {
-    pub fn is_selected(&self) -> bool {
-        self.is_selected
+impl<T: Display + Clone> Option<T> {
+    pub fn is_checked(&self) -> bool {
+        self.checked
     }
 }
 
-impl<T: Display + Clone> Line<T> {
+impl<T: Display + Clone> Option<T> {
+    pub fn is_highlighted(&self) -> bool {
+        self.checked
+    }
+}
+
+impl<T: Display + Clone> Option<T> {
     pub fn toggle(&mut self) {
-        self.is_selected = !self.is_selected
+        self.checked = !self.checked
     }
 }
 
-impl<T: Display + Clone> Line<T> {
+impl<T: Display + Clone> Option<T> {
     pub fn get_content(self) -> T {
         self.content.clone()
     }
 }
 
-impl<T: Display + Clone> Display for Line<T> {
+impl<T: Display + Clone> Display for Option<T> {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        let checked = String::from("🟢");
-        let unchecked = String::from("⚪");
+        let checked = String::from("[x]");
+        let unchecked = String::from("[ ]");
         let content = &self.content;
-        if self.is_selected {
+        if self.checked {
             write!(f, "{:<2} {}", checked, content)
         } else {
             write!(f, "{:<2} {}", unchecked, content)
--- src/display.rs
+++ src/display.rs
@@ -1,6 +1,6 @@
-use crate::def::Line;
+use crate::def::Option;
 
-pub fn display<T: std::fmt::Display + Clone>(lines: &[Line<T>], current: &usize) {
+pub fn display<T: std::fmt::Display + Clone>(lines: &[Option<T>], current: &usize) {
     for (index, line) in lines.iter().enumerate() {
         if index == *current {
             println!("❯ {}", line);
