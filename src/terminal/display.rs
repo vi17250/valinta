88ca2de288c13de6c959064bd7195739a9af4de0
vi17250 <v.aubinaud@protonmail.com>
2025-12-09 21:21:05 +0100

    fix: 🐛 current line is highlighted

--- src/terminal/display.rs
+++ src/terminal/display.rs
@@ -4,7 +4,7 @@ use console::style;
 pub fn display<T: std::fmt::Display + Clone>(lines: &Vec<Line<T>>) {
     for line in lines {
         let render = if line.is_highlighted() {
-            style(line).bold()
+            style(line).on_green()
         } else {
             style(line)
         };
