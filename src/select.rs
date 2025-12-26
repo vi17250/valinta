1501165a0621311215958c9f3ef8136b71af6293
vi17250 <v.aubinaud@protonmail.com>
2025-12-26 18:21:43 +0100

    feat: 🎸 Move cursor
    BREAKING CHANGE: 🧨 When a list is longer than max lines to render so the first are fixed
    and after the lit move interactivelly
    
    ✅ Closes: #32

--- src/select.rs
+++ src/select.rs
@@ -10,10 +10,10 @@ use crate::utils::{array, number_of_rendered_lines};
 
 pub type Returned<T> = (Vec<T>, Vec<usize>);
 
-const NUMBER_TO_RENDER: usize = 11;
+const NUMBER_TO_RENDER: usize = 7;
 
 /// `select` is the main function of this crate
-/// 
+///
 /// This function orchestrate the entier logic of this crate
 /// It's not recommened to use it dry
 pub fn select<T: Display + Clone>(items: &[T]) -> Result<Returned<T>> {
@@ -21,16 +21,16 @@ pub fn select<T: Display + Clone>(items: &[T]) -> Result<Returned<T>> {
         return Err(ValintaError::ItemsCannotBeEmpty);
     }
 
+    let mut offset = 0;
+    let mut cursor_position = 0;
+
     let mut lines = items
         .iter()
         .enumerate()
-        .map(|(index, option)| Line::new(option, index == 0))
+        .map(|(index, item)| Line::new(item, index == cursor_position))
         .collect::<Vec<Line<&T>>>();
 
-
-    let mut cursor_position = 0;
-
-    let mut rendered_lines = array(&lines, Some(cursor_position), Some(NUMBER_TO_RENDER));
+    let mut rendered_lines = array(&lines, Some(offset), Some(NUMBER_TO_RENDER));
 
     display(&rendered_lines);
 
@@ -47,7 +47,17 @@ pub fn select<T: Display + Clone>(items: &[T]) -> Result<Returned<T>> {
         match key {
             Key::Enter => break,
             Key::Escape => process::exit(0),
-            key_pressed => use_key(&mut lines, key_pressed),
+            key_pressed => {
+                if key_pressed == Key::ArrowDown && cursor_position < NUMBER_TO_RENDER / 2 {
+                    cursor_position += 1;
+                } else if key_pressed == Key::ArrowUp
+                    && cursor_position > 0
+                    && cursor_position < NUMBER_TO_RENDER / 2
+                {
+                    cursor_position -= 1;
+                }
+                use_key(&mut lines, key_pressed)
+            }
         }
 
         let terminal_width = get_width()?;
@@ -57,13 +67,24 @@ pub fn select<T: Display + Clone>(items: &[T]) -> Result<Returned<T>> {
         let _ = term.move_cursor_up(number_of_rendered_lines);
         let _ = term.clear_to_end_of_screen();
 
-        cursor_position = if lines.len() < NUMBER_TO_RENDER {
+            
+        let index_of_highlighted = lines.iter().position(|line| line.is_highlighted()).unwrap();
+
+        offset = if lines.len() > NUMBER_TO_RENDER && cursor_position == 0 {
+            index_of_highlighted
+        } else if lines.len() <= NUMBER_TO_RENDER || cursor_position < NUMBER_TO_RENDER / 2 {
             0
         } else {
-            lines.iter().position(|line| line.is_highlighted()).unwrap()
+            let len_after_cursor_position = items.len() - NUMBER_TO_RENDER / 2;
+            let size = len_after_cursor_position + index_of_highlighted;
+            if size > lines.len() {
+                size - lines.len()
+            } else {
+                size
+            }
         };
 
-        rendered_lines = array(&lines, Some(cursor_position), Some(NUMBER_TO_RENDER));
+        rendered_lines = array(&lines, Some(offset), Some(NUMBER_TO_RENDER));
 
         display(&rendered_lines);
     }
